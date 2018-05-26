e3d89e9941074f53fd816edb4e3abf87f1299529
Arni Geir Sigurdsson <arni.geir.sigurdsson@gmail.com>
2018-05-26 20:07:22 +0000

    reverted changes on master

--- src/main/webapp/scripts/App4Sea.Map.OpenLayers.js
+++ src/main/webapp/scripts/App4Sea.Map.OpenLayers.js
@@ -11,15 +11,16 @@ App4Sea.Map.OpenLayers = (function(){
     var esriWSPTileLayer;
     var esriWITileLayer;
     var that = {};
+    var areaCenter = [67.5085683629386,-3.251953125];
     var initialZoom = 4;
-    //var areaCenter = [67.5085683629386,-3.251953125];
-    //var npaLayer = null;
-    //var MapTileLayer = null;
-    //function onMapClick(e) {
-    //   $("#DebugWindow").append("["+e.latlng.lat+","+e.latlng.lng+"],<br/>");
-    //} 
+    var npaLayer = null;
+    var MapTileLayer = null;
+    function onMapClick(e) {
+       $("#DebugWindow").append("["+e.latlng.lat+","+e.latlng.lng+"],<br/>");
+    } 
+    
+    
 
-    ////////////////////////////////////////////////////////////////////////////
     //initialize maps and models when page DOM is ready..
     function init(){
         //init Leaflet map with MapBox tiles on Reykjavik
@@ -30,37 +31,16 @@ App4Sea.Map.OpenLayers = (function(){
               zoom: initialZoom
             })
           });
-      
-        // Init osmTileLayer base map
+          
         osmTileLayer =   new ol.layer.Tile({source: new ol.source.OSM()});
-        osmTileLayer.name = "osmTileLayer";
-
-        // Init esriWSPTileLayer base map
+        currentLayer = osmTileLayer;
+        
         var attributionESRIWSM = new ol.Attribution({
             html: 'Tiles &copy; <a href="https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/0">ArcGIS</a>'
         });
-                        
-        esriWSPTileLayer = new ol.layer.Tile({
-            source: new ol.source.XYZ({
-              attributions: [attributionESRIWSM],
-              url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
-            })
-        });  
-        esriWSPTileLayer.name = "esriWSPTileLayer";
         
-        // Init esriWITileLayer base map
-        var attributionWIM = new ol.Attribution({
-            html: 'Tiles &copy; <a href="https://http://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/0">ArcGIS</a>'
-        });
-        esriWITileLayer = new ol.layer.Tile({
-            source: new ol.source.XYZ({
-              attributions: [attributionWIM],
-              url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-            })
-        });  
-        esriWITileLayer.name = "esriWITileLayer";
         
-        // Define function for click
+        
         var displayFeatureInfo = function (pixel) {
             var container =  $("#FeatureInfoContainer");
             container.empty();
@@ -101,36 +81,54 @@ App4Sea.Map.OpenLayers = (function(){
                     description += "</tbody></table>";
                     container.append(description).show();
                 }
-            }
-        };
 
+            }
+
+          
+        };
+        
         myMap.on('click', function(evt) {            
             displayFeatureInfo(evt.pixel);
-        });      
-
-        // Set current base map layer
-        currentLayer = esriWSPTileLayer;
+        });
+   
+        
+        
+        esriWSPTileLayer = new ol.layer.Tile({
+            source: new ol.source.XYZ({
+              attributions: [attributionESRIWSM],
+              url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}'
+            })
+        });  
+        
+        var attributionWIM = new ol.Attribution({
+            html: 'Tiles &copy; <a href="https://http://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/0">ArcGIS</a>'
+        });
+        esriWITileLayer = new ol.layer.Tile({
+            source: new ol.source.XYZ({
+              attributions: [attributionWIM],
+              url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+            })
+        });  
+        
+        
+          
         myMap.addLayer(currentLayer);
 
-        // Add controls
+        
         var zoomslider = new ol.control.ZoomSlider();
-        myMap.addControl(zoomslider);        
+        myMap.addControl(zoomslider); 
         
-        heatmap();
         
-        // Hook events to menu
+        
         $(".MenuSection input[type='checkbox']").click(function(){
             update();
         });
         $(".MenuSection select").change(function(){
             update();
         });
-        
-        // Update the page
         update();
+        
     };
-
-    ////////////////////////////////////////////////////////////////////////////
     //load kml and return as Vector
     function loadKml(url){
         var vector = new ol.layer.Vector({
@@ -146,72 +144,21 @@ App4Sea.Map.OpenLayers = (function(){
         return vector;
     }
     
-    ////////////////////////////////////////////////////////////////////////////
-    // Init layer vectors
     var npaVector = loadKml('data/NPA.kml');
     var medDeccVector = loadKml('data/DECC_OFF_Median_Line.kml');
     var hydDeccVector = loadKml('data/DECC_OFF_Hydrocarbon_Fields.kml');
-    var insidentsVector = loadKml('data/Incidents.kml');
+    var insidentsVector = loadKml('data/incidents.kml');
     var meetingsVector = loadKml('data/MeetingPlaces.kml');
     var nyBeredskapsdepotVector = loadKml('data/ny_beredskapsdepot.kml');
-
-    ////////////////////////////////////////////////////////////////////////////
-    // HeatMap
-    function heatmap () {
-        var blur = document.getElementById('blur');
-        var radius = document.getElementById('radius');
-
-        var vector = new ol.layer.Heatmap({
-          source: new ol.source.Vector({
-            url: 'https://openlayers.org/en/v4.6.5/examples/data/kml/2012_Earthquakes_Mag5.kml',
-            format: new ol.format.KML({
-              extractStyles: false
-            })
-          }),
-          blur: parseInt(blur.value, 10),
-          radius: parseInt(radius.value, 10)
-        });
-
-        vector.getSource().on('addfeature', function(event) {
-          // 2012_Earthquakes_Mag5.kml stores the magnitude of each earthquake in a
-          // standards-violating <magnitude> tag in each Placemark.  We extract it from
-          // the Placemark's name instead.
-          var name = event.feature.get('name');
-          var magnitude = parseFloat(name.substr(2));
-          event.feature.set('weight', magnitude - 5);
-        });
-
-        var raster = new ol.layer.Tile({
-          source: new ol.source.Stamen({
-            layer: 'toner'
-          })
-        });
-
-/*        var map = new ol.Map({
-          layers: [raster, vector],
-          target: 'map',
-          view: new ol.View({
-            center: [0, 0],
-            zoom: 2
-          })
-        });*/
-        myMap.addLayer(vector);
-
-        blur.addEventListener('input', function() {
-          vector.setBlur(parseInt(blur.value, 10));
-        });
-
-        radius.addEventListener('input', function() {
-          vector.setRadius(parseInt(radius.value, 10));
-        });
-    }
     
-    ////////////////////////////////////////////////////////////////////////////
-    // Update layers
+    
+           
+
+
     function update(){
-        // Set base map
+        
         var selectedMapLayer = $("#MenuLayer_Select").val();
-        if(selectedMapLayer !== currentLayer.name){
+        if(selectedMapLayer !== currentLayer){
             myMap.removeLayer(currentLayer);
             if(selectedMapLayer === 'osmTileLayer'){
                 currentLayer = osmTileLayer;
@@ -223,12 +170,14 @@ App4Sea.Map.OpenLayers = (function(){
             myMap.addLayer(currentLayer);
         }
         
-        //add NPA layers if selected
+        
+        //add NPA layer if selected
+        
+        var customLayer;
         myMap.removeLayer(npaVector);
         if($("#MenuNPALayer_Checkbox").is(":checked") && npaVector){
             myMap.addLayer(npaVector);           
         }
-            
         myMap.removeLayer(medDeccVector);
         if($("#MenuMedDeccLayer_Checkbox").is(":checked") && medDeccVector){
             myMap.addLayer(medDeccVector);           
@@ -238,7 +187,6 @@ App4Sea.Map.OpenLayers = (function(){
         if($("#MenuHydDeccLayer_Checkbox").is(":checked") && hydDeccVector){
             myMap.addLayer(hydDeccVector);           
         }    
-
         myMap.removeLayer(meetingsVector);
         if($("#MenuMedMeetings_Checkbox").is(":checked") && meetingsVector){
             myMap.addLayer(meetingsVector);           
@@ -252,8 +200,12 @@ App4Sea.Map.OpenLayers = (function(){
         myMap.removeLayer(nyBeredskapsdepotVector);
         if($("#MenuBeredskapsdepot_Checkbox").is(":checked") && nyBeredskapsdepotVector){
             myMap.addLayer(nyBeredskapsdepotVector);           
-        }
+        } 
+        
+        
+        
     }
+    
     
     that.Init = init;
 
